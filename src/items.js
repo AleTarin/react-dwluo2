22671e719838dabbfdbd0dc4a1f1d94be1eb0e41
Alejandro De la Cruz Tarin <alejandro.delacruztarin@coxautoinc.com>
2020-11-24 13:47:33 -0600

    test

---
+++ src/items.js
@@ -0,0 +1,104 @@
+export default [
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description: "VIDEO_WALKAROUND_VERBIAGE",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-video-form.htm",
+    linkTarget: "dialog",
+    linkText: "SCHEDULE_A_VIDEO_CALL",
+    title: "VIDEO_WALKAROUND"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description:
+      "AUTO_USED_DETAILS_DEALER_SERVICES1_DESCRIPTION__STATIC_ASSETS_IMAGES_DEALER_SERVICES_TEST_DRIVE_PNG",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-test-drive-home-form.htm",
+    linkTarget: "dialog",
+    linkText: "REQUEST_A_TEST_DRIVE",
+    title: "TEST_DRIVE_AT_HOME"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description: "HOME_DELIVERY_VERBIAGE",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-delivery-form.htm",
+    linkTarget: "dialog",
+    linkText: "ASK_ABOUT_DELIVERY",
+    title: "HOME_DELIVERY"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description: "VIDEO_WALKAROUND_VERBIAGE",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-video-form.htm",
+    linkTarget: "dialog",
+    linkText: "SCHEDULE_A_VIDEO_CALL",
+    title: "VIDEO_WALKAROUND"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description:
+      "AUTO_USED_DETAILS_DEALER_SERVICES1_DESCRIPTION__STATIC_ASSETS_IMAGES_DEALER_SERVICES_TEST_DRIVE_PNG",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-test-drive-home-form.htm",
+    linkTarget: "dialog",
+    linkText: "REQUEST_A_TEST_DRIVE",
+    title: "TEST_DRIVE_AT_HOME"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description: "HOME_DELIVERY_VERBIAGE",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-delivery-form.htm",
+    linkTarget: "dialog",
+    linkText: "ASK_ABOUT_DELIVERY",
+    title: "HOME_DELIVERY"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description: "VIDEO_WALKAROUND_VERBIAGE",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-video-form.htm",
+    linkTarget: "dialog",
+    linkText: "SCHEDULE_A_VIDEO_CALL",
+    title: "VIDEO_WALKAROUND"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description:
+      "AUTO_USED_DETAILS_DEALER_SERVICES1_DESCRIPTION__STATIC_ASSETS_IMAGES_DEALER_SERVICES_TEST_DRIVE_PNG",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-test-drive-home-form.htm",
+    linkTarget: "dialog",
+    linkText: "REQUEST_A_TEST_DRIVE",
+    title: "TEST_DRIVE_AT_HOME"
+  },
+  {
+    entityComponentClassName: "com.dealer.cms.model.widgets.DealerServices",
+    description: "HOME_DELIVERY_VERBIAGE",
+    deviceOverrides: {},
+    imageSrc: "https://images.dealer.com/ddc/lincoln/support.png",
+    itemEnabled: true,
+    linkHref: "/schedule-delivery-form.htm",
+    linkTarget: "dialog",
+    linkText: "ASK_ABOUT_DELIVERY",
+    title: "HOME_DELIVERY"
+  }
+];
